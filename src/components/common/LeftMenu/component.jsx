5ef1a5dbb9113ae24af94893932408b01a166735
Dzmitry Bobr <dmitry.bobr@cogniteq.com>
2020-01-13 17:23:20 +0300

    Add LeftMenu Component Add basic components structure for LeftMenu Component Add Categories component skeleton for LeftMenu Component

---
+++ src/components/common/LeftMenu/component.jsx
@@ -0,0 +1,12 @@
+import React, {PureComponent} from 'react';
+import './styles.scss';
+
+class LeftMenu extends PureComponent {
+  render() {
+    return (
+      <div/>
+    )
+  }
+}
+
+export default LeftMenu;
